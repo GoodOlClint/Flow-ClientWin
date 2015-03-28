3b7282926c78b6793a073f8d519c6aaa008cd27b
GoodOlClint <goodolclint@gmail.com>
2015-03-28 12:13:58 -0500

    VS Solution Setup

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flow_ClientWin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Signals.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flow_ClientWin
+{
+    public class Signal<T> where T : SignalProvider
+    {
+        public TimeSpan Age { get; set; }
+        public string Path { get; set; }
+        public string Value { get; set; }
+    }
+
+    public abstract class SignalProvider
+    {
+        
+    }
+}
